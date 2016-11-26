22c97cc5697fc879fc9dd886e400a3acb7114dfd
Johannes B <do@not.tell>
2016-11-26 16:25:51 +0100

    add simple value checker to sax handler

--- src/main/java/de/htwg/msi/infe/ermcompile/utils/XmlSaxHandler.java
+++ src/main/java/de/htwg/msi/infe/ermcompile/utils/XmlSaxHandler.java
@@ -7,11 +7,15 @@ import de.htwg.msi.infe.ermcompile.model.XML.RelationtypeXML;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.Locator;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class XmlSaxHandler extends DefaultHandler {
+
+    //locator to get current position
+    private Locator locator;
 
     //xml specifig tags
     private final String entityTypes = "entitytypes";
@@ -55,7 +59,7 @@ public class XmlSaxHandler extends DefaultHandler {
     public List<RelationtypeXML> getRelationtypeXMLList() {
         return this.relationtypeXMLList;
     }
-    
+
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 
@@ -65,12 +69,16 @@ public class XmlSaxHandler extends DefaultHandler {
 
         if (qName.equalsIgnoreCase(this.entityType)) {
             this.tempEntity = new EntityTypeXML();
+            this.checkValidTag(attributes.getValue(this.name), this.name, this.locator);
+            System.out.println();
             this.tempEntity.setName(attributes.getValue(this.name));
             System.out.println("\nentitytype : " + attributes.getValue(this.name));
         }
         if (qName.equalsIgnoreCase(this.attribute) && (this.inEntitytypes || this.inRelationtypes)) {
             this.inAttribute = true;
             this.tempAttribute = new AttributesXML();
+            this.checkValidTag(attributes.getValue(this.isprimkey), this.isprimkey, locator);
+            this.checkValidTag(attributes.getValue(this.notnull), this.notnull, locator);
             this.tempAttribute.setIsprimkey(Boolean.parseBoolean(attributes.getValue(this.isprimkey)));
             this.tempAttribute.setNotnull(Boolean.parseBoolean(attributes.getValue(this.notnull)));
             System.out.print("attribute: isprimkey:" + attributes.getValue(this.isprimkey) + "| notnull: " + attributes.getValue(this.notnull));
@@ -81,6 +89,7 @@ public class XmlSaxHandler extends DefaultHandler {
 
         if (qName.equalsIgnoreCase(this.relationType)) {
             this.tempRelation = new RelationtypeXML();
+            this.checkValidTag(attributes.getValue(this.name), name, locator);
             this.tempRelation.setName(attributes.getValue(this.name));
             System.out.println("\nrelationtype : " + attributes.getValue(this.name));
         }
@@ -88,6 +97,8 @@ public class XmlSaxHandler extends DefaultHandler {
         if (qName.equalsIgnoreCase(this.entityLink)) {
             this.inLink = true;
             this.tempLink = new LinksXML();
+            this.checkValidTag(attributes.getValue(this.min), this.min, this.locator);
+            this.checkValidTag(attributes.getValue(this.max), this.max, this.locator);
             this.tempLink.setMin(attributes.getValue(this.min));
             this.tempLink.setMax(attributes.getValue(this.max));
             this.tempLink.setFunctionality(attributes.getValue(this.funct));
@@ -96,17 +107,20 @@ public class XmlSaxHandler extends DefaultHandler {
         }
     }
 
+
     @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
         if (this.inAttribute) {
             String name = new String(ch, start, length);
             System.out.print(" name: " + name + "\n");
+            this.checkValidTag(name, this.name, this.locator);
             this.tempAttribute.setName(name);
             this.inAttribute = false;
         }
         if (this.inLink) {
             String name = new String(ch, start, length);
             System.out.print(" name: " + name + "\n");
+            this.checkValidTag(name, this.name, this.locator);
             this.tempLink.setName(name);
             this.inLink = false;
         }
@@ -135,6 +149,34 @@ public class XmlSaxHandler extends DefaultHandler {
         if (qName.equalsIgnoreCase(this.entityTypes)) {
             this.inRelationtypes = false;
         }
+    }
+
+    @Override
+    public void setDocumentLocator(Locator locator) {
+        this.locator = locator;
+    }
+
+
+    private void checkValidTag(String s, String tagType, Locator locator) throws SAXException {
+
+        if (tagType.equals(this.name)) {
+            if (s.isEmpty() || s.trim().isEmpty()) {
+                throw new SAXException("XML Tag Error:\n '"
+                        + tagType + "' or value of Element cant be empty in line: " + locator.getLineNumber());
+            }
+        }
+        if (tagType.equals(this.notnull) || tagType.equals(this.isprimkey)) {
+            if (!s.equals("true") && !s.equals("false")) {
+                throw new SAXException("XML Tag Error:\n '"
+                        + tagType + "' must be either 'true' or 'false' but is '" + s + "' in line: " + locator.getLineNumber());
+            }
+        }
+
+        if (tagType.equals(this.min) || tagType.equals(this.max)) {
+            //TODO check min max values in tags
+        }
+
+
     }
 }
 
