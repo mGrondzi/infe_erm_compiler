8170efadf30e9ef92939cc72c6db9cb67cac057d
Philip Schaefer <philip.schaefer@htwg-konstanz.de>
2016-11-29 18:36:51 +0100

    Adding binary table combination cases for 1:1 relations

--- src/main/java/de/htwg/msi/infe/ermcompile/logic/RelationResolver.java
+++ src/main/java/de/htwg/msi/infe/ermcompile/logic/RelationResolver.java
@@ -19,13 +19,52 @@ public class RelationResolver {
     public RelationResolver(Erm erm) {
         this.erm = erm;
         this.rtables = this.erm.getTables();
+    }
+
+    public void resolve() {
 
         this.rtables = this.extractRelationTypes(rtables);
 
         for (Table table : this.rtables) {
-            System.out.println(table.getName());
+            if (this.isRLBinary((Relationtype) table)) {
+                this.combineBinaryRelation((Relationtype) table);
+            } else {
+                // combine ternary relations
+            }
         }
+    }
 
+    private void combineBinaryRelation(Relationtype rt) {
+        ArrayList<EntityLink> links = rt.getLinks();
+        EntityLink linkLeft = rt.getLinks().get(0);
+        EntityLink linkRight = rt.getLinks().get(1);
+
+        if (linkLeft.getFunctionality() == "1" && linkRight.getFunctionality() == "1") {
+            if (linkLeft.getCardinality().getMin() == "1" && linkLeft.getCardinality().getMax() == "1") {
+                if (linkRight.getCardinality().getMin() == "1" && linkRight.getCardinality().getMax() == "1") {
+                    // L(1,1)--(1,1)R
+                    //Fasse beide in einer Zusammen
+                } else {
+                    // L(1,1)--(0,1)R
+                    // Packe PK von R als FK in L
+                }
+            } else {
+                if (linkRight.getCardinality().getMin() == "1" && linkRight.getCardinality().getMax() == "1") {
+                    // L(0,1)--(1,1)R
+                    // Packe PK von L als FK in R
+                } else {
+                    // L(0,1)--(0,1)R
+                    // Erstelle Neue Tabelle PK ist PK(L)+PK(R)
+                }
+            }
+
+        } else if (linkLeft.getFunctionality() == "1" && linkRight.getFunctionality() == "N") {
+
+        } else if (linkLeft.getFunctionality() == "N" && linkRight.getFunctionality() == "1") {
+
+        } else {// N M
+
+        }
     }
 
     public ArrayList<Table> extractEntityTypes(ArrayList<Table> list) {
@@ -38,7 +77,7 @@ public class RelationResolver {
         return en;
     }
 
-    public ArrayList<Table> extractRelationTypes(ArrayList<Table> list) {
+    private ArrayList<Table> extractRelationTypes(ArrayList<Table> list) {
         ArrayList<Table> rl = new ArrayList<Table>();
         for (Table table : list) {
             if (table instanceof Relationtype) {
@@ -52,5 +91,12 @@ public class RelationResolver {
         ArrayList<EntityLink> link = rl.getLinks();
     }
 
+    private boolean isRLBinary(Relationtype rl) {
+
+        if (rl.getLinks().size() == 2) {
+            return true;
+        }
+        return false;
+    }
 
 }
